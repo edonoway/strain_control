67801fdd67346f0a2f980b92c1b8cae1649fb63b
Alex Liebman-Pelaez <alexliebmanp@gmail.com>
2022-07-03 17:15:28 -0700

    added MIT Licenese

--- example_notebook.py
+++ example_notebook.py
@@ -9,11 +9,7 @@ sc = StrainClient()
 sc.start_strain_control('Combined')
 sc.change_setpoint(0.07)
 sc.get_voltage(1)
+sc.get_strain()
 sc.change_slew_rate(10)
 sc.set_voltage(1,-10)
 sc.shutdown_server()
-
-import re
-string = 'SCTRL:3'
-re.match(r'SCTRL:[1-3]', string)
-re.search(r'[1-3]', string)
